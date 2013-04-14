e2df9a3e8e3d571aade2b0a89f0da80af9b89738
buyhome <pai6@qq.com>
2013-04-14 18:27:37 +0800

    mqfare client of android

--- src/ifl/queues/coqueues.java
+++ src/ifl/queues/coqueues.java
@@ -7,6 +7,7 @@ import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.entity.UrlEncodedFormEntity; 
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost; 
+import org.apache.http.entity.AbstractHttpEntity;
 import org.apache.http.impl.client.DefaultHttpClient; 
 import org.apache.http.message.BasicNameValuePair; 
 import org.apache.http.protocol.HTTP;
@@ -16,12 +17,18 @@ import org.apache.http.util.EntityUtils;
 /*必需引用java.io 与java.util相关类来读写文件*/
 import irdc.EX08_01.R;
 
+import java.io.BufferedReader;
 import java.io.IOException; 
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList; 
 import java.util.List; 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import android.R.string;
 import android.app.Activity; 
 import android.os.Bundle; 
 import android.view.View; 
@@ -32,12 +39,13 @@ import android.widget.TextView;
 import pk.aamir.stompj.*;
 import pk.aamir.stompj.internal.*;
 
+
 public class coqueues extends Activity 
 { 
   /*声明一个Button对象,与一个TextView对象*/
   private Button mButton1;
   private Button mButton2;
-  private TextView mTextView1; 
+  private TextView mTextView1;
    
   /** Called when the activity is first created. */ 
   @Override 
@@ -57,39 +65,84 @@ public class coqueues extends Activity
       @Override 
       public void onClick(View v) 
       { 
+        /*MQ*/
+        Connection con = new Connection("10.124.20.135", 61613, "admin", "admin");
+        
         // TODO Auto-generated method stub 
         /*声明网址字符串*/
-        //Connection con = new Connection("192.168.13.111", 61612, "admin", "admin");
-        Connection con = new Connection("10.124.20.49", 61616);
-        try
-        {
-          con.connect();
+        String uriAPI = "http://rhomobi.com/api/nodes.json"; 
+        /*建立HTTP Get联机*/
+        HttpGet httpRequest = new HttpGet(uriAPI); 
+        try 
+          { 
+            /*发出HTTP request*/
+            HttpResponse httpResponse = new DefaultHttpClient().execute(httpRequest); 
+            /*若状态码为200 ok*/
+            if(httpResponse.getStatusLine().getStatusCode() == 200)  
+            { 
+              /*取出响应字符串*/
+              String strResult = EntityUtils.toString(httpResponse.getEntity());
+              /*删除多余字符*/
+              strResult = eregi_replace("(\r\n|\r|\n|\n\r)","",strResult);
+               
+              
+              try
+              {
+                con.connect();
+                DefaultMessage msg = new DefaultMessage();
 
-          DefaultMessage msg = new DefaultMessage();
-          msg.setProperty("type", "text/plain");
-          msg.setContent("Another test message!");
-          con.send(msg, "/queue/ifl_ticket");
+                //msg.setProperty("type", "text/plain");
+                msg.setProperty("type", "application/json");
+                System.out.println("++++++++++++++++++\n"+strResult);
+                msg.setContent(strResult);
+                con.send(msg, "/queue/ifl_ticket");
+  
+                con.disconnect();
+                
+                mTextView1.setText(strResult);
+                
+                 
+                
+              } catch (StompJException e)
+              {
+                // TODO Auto-generated catch block
+                mTextView1.setText(e.getMessage().toString());
+                e.printStackTrace();
+              }
+              
 
-          con.disconnect();
-          
-        } catch (StompJException e)
-        {
-          // TODO Auto-generated catch block
-          mTextView1.setText(e.getMessage().toString());
-          e.printStackTrace();
-        }
+            } 
+            else 
+            { 
+              mTextView1.setText("Error Response: "+httpResponse.getStatusLine().toString()); 
+            } 
+          } 
+          catch (ClientProtocolException e) 
+          {  
+            mTextView1.setText(e.getMessage().toString()); 
+            e.printStackTrace(); 
+          } 
+          catch (IOException e) 
+          {  
+            mTextView1.setText(e.getMessage().toString()); 
+            e.printStackTrace(); 
+          } 
+          catch (Exception e) 
+          {  
+            mTextView1.setText(e.getMessage().toString()); 
+            e.printStackTrace();  
+          }  
+
       }
     }); 
     
     mButton1.setOnClickListener(new Button.OnClickListener() 
     { 
-      @Override 
-      public void onClick(View v) 
-      { 
+    @Override 
+    public void onClick(View v) { 
         // TODO Auto-generated method stub 
         /*声明网址字符串*/
-        //Connection con = new Connection("192.168.13.111", 61612, "admin", "admin");
-        Connection con = new Connection("10.124.20.49", 61616);
+        Connection con = new Connection("10.124.20.135", 61613, "admin", "admin");
         try
         {
           con.connect();
@@ -100,59 +153,27 @@ public class coqueues extends Activity
               //System.out.println(msg.getContentAsString());
               String strResult = msg.getContentAsString();
               strResult = eregi_replace("(\r\n|\r|\n|\n\r)","",strResult);
-              mTextView1.setText(strResult);
+              //mTextView1.setText(strResult);              
               
               /*声明网址字符串*/
               String uriAPI = "http://10.124.20.136/data-base";
-              /*建立HTTP Post联机*/
-              HttpPost httpRequest = new HttpPost(uriAPI); 
-              /*
-               * Post运作传送变量必须用NameValuePair[]数组储存
-               * 2013/01/19 19:27:48 [debug] 18229#0: *14 http header: "Content-Type: application/x-www-form-urlencoded"
-               * 2013/01/19 19:27:48 [debug] 18229#0: *14 http header: "Host: labs.rhomobi.com"
-               * 2013/01/19 19:27:48 [debug] 18229#0: *14 http header: "Connection: Keep-Alive"
-               * 2013/01/19 19:27:48 [debug] 18229#0: *14 http header: "User-Agent: Apache-HttpClient/UNAVAILABLE (java 1.4)"
-              */
-              List <NameValuePair> params = new ArrayList <NameValuePair>(); 
-              //params.add(new BasicNameValuePair("str", "I am Post String"));
-              params.add(new BasicNameValuePair("body", strResult));
-              try 
-              { 
-                /*发出HTTP request*/
-                httpRequest.setEntity(new UrlEncodedFormEntity(params, HTTP.UTF_8)); 
-                /*取得HTTP response*/
-                HttpResponse httpResponse = new DefaultHttpClient().execute(httpRequest); 
-                /*若状态码为200 ok*/
-                if(httpResponse.getStatusLine().getStatusCode() == 200)  
-                { 
-                  /*取出响应字符串*/
-                  String strResultPost = EntityUtils.toString(httpResponse.getEntity());
-                  mTextView1.setText(strResultPost); 
-                } 
-                else 
-                { 
-                  mTextView1.setText("Error Response: "+httpResponse.getStatusLine().toString()); 
-                } 
-              } 
-              catch (ClientProtocolException e) 
-              {  
-                mTextView1.setText(e.getMessage().toString()); 
-                e.printStackTrace(); 
-              } 
-              catch (IOException e) 
-              {  
-                mTextView1.setText(e.getMessage().toString()); 
-                e.printStackTrace(); 
-              } 
-              catch (Exception e) 
-              {  
-                mTextView1.setText(e.getMessage().toString()); 
-                e.printStackTrace();  
-              }  
-              
-            }
+              try
+              {
+                String res = postfareasraw(uriAPI,strResult);
+                //String res = simplepostfare(uriAPI,strResult);
+                System.out.println("++++++++++++++++++:\n"+res);
+                
+                //mTextView1.setText(res);
+              } catch (Exception e)
+              {
+                // TODO Auto-generated catch block
+                mTextView1.setText(e.getMessage().toString());
+                e.printStackTrace();
+              }             
+             
+            }   
           });
-          
+
           //con.disconnect();
           
         } catch (StompJException e)
@@ -178,7 +199,76 @@ public class coqueues extends Activity
       {
         return strTarget;
       }
+    }    
+    
+    public String postfareasraw(String purl, String faredata) throws Exception{
+      HttpURLConnection con = (HttpURLConnection)new URL(purl).openConnection();
+      con.setRequestMethod("POST");
+      con.setDoOutput(true);
+      con.setDoInput(true);
+      con.setUseCaches(false);
+      con.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+      OutputStreamWriter osw = new OutputStreamWriter(con.getOutputStream(),"UTF-8");
+      
+      osw.write(faredata);
+      osw.flush();
+      osw.close();
+      
+      //读取返回信息
+      StringBuffer buffer =  new StringBuffer();
+      BufferedReader br = null;
+      br = new BufferedReader(new InputStreamReader(con.getInputStream(),"UTF-8"));
+      String temp;
+      while ((temp = br.readLine()) != null) {
+       buffer.append(temp);
+       buffer.append("\n");
+      }
+      //System.out.println("++++++++++++++++++:"+buffer.toString());
+
+      return buffer.toString();
     }
     
+    public String simplepostfare(String purl, String faredata){
+        HttpPost httpRequest = new HttpPost(purl); 
+        /*  
+        * Post运作传送变量必须用NameValuePair[]数组储存
+        * 2013/01/19 19:27:48 [debug] 18229#0: *14 http header: "Content-Type: application/x-www-form-urlencoded"
+        * 2013/01/19 19:27:48 [debug] 18229#0: *14 http header: "Host: labs.rhomobi.com"
+        * 2013/01/19 19:27:48 [debug] 18229#0: *14 http header: "Connection: Keep-Alive"
+        * 2013/01/19 19:27:48 [debug] 18229#0: *14 http header: "User-Agent: Apache-HttpClient/UNAVAILABLE (java 1.4)"
+        */
+       List <NameValuePair> params = new ArrayList <NameValuePair>(); 
+       //params.add(new BasicNameValuePair("str", "I am Post String"));
+       params.add(new BasicNameValuePair("body", faredata));
+       try 
+       { 
+       httpRequest.setEntity(new UrlEncodedFormEntity(params, HTTP.UTF_8)); 
+  
+       HttpResponse httpResponse = new DefaultHttpClient().execute(httpRequest); 
+  
+       if(httpResponse.getStatusLine().getStatusCode() == 200)
+         { 
+           String strResultPost = EntityUtils.toString(httpResponse.getEntity());
+           //mTextView1.setText(strResultPost); 
+           return strResultPost;
+         } 
+       else 
+         { 
+           //mTextView1.setText("Error Response: "+httpResponse.getStatusLine().toString()); 
+           return "Error Response: "+httpResponse.getStatusLine().toString();
+         } 
+       } 
+       catch (ClientProtocolException e) 
+       {  
+         mTextView1.setText(e.getMessage().toString()); 
+         e.printStackTrace(); 
+       } 
+       catch (IOException e) 
+       {  
+         mTextView1.setText(e.getMessage().toString()); 
+         e.printStackTrace(); 
+       } 
+       return "";
+    }
     
 }
